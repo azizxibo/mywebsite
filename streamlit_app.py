4fc88157057cc5d3d1af6d9be7d1d87a01e296e6
Aziz Ramdhani <aziz.ramdhani@student.president.ac.id>
2024-12-24 21:18:07 +0700

    add tittle and form

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
-st.title("🎈 welcome to my website azizzz")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Aplikasi Undangan Pernikahan Online")
+with st.form("undangan_form"):
+    nama = st.text_input("Nama Pengantin")
+    tanggal = st.date_input("Tanggal Pernikahan")
+    lokasi = st.text_input("Lokasi Pernikahan")
+    submit_button = st.form_submit_button("Buat Undangan")
+
+if submit_button:
+    st.write(f"**Undangan Pernikahan**")
+    st.write(f"Nama: {nama}")
+    st.write(f"Tanggal: {tanggal}")
+    st.write(f"Lokasi: {lokasi}")
